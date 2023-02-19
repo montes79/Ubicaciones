e4bc02359cdfb2cbdd57c2c7c2252c297dbc4814
Luis Montes <montes_luisalberto79@hotmail.com>
2023-02-19 14:26:29 -0600

    [fix] Filtro de imagen correcto
    Se filtra la imagen que debe ser portada del listado que trae la receta. No es la primera, puede ser cualquiera de las 3 imagenes que trae

--- app/src/main/java/com/luis/montes/ubicaciones/vistas/RecetasHolder.kt
+++ app/src/main/java/com/luis/montes/ubicaciones/vistas/RecetasHolder.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.DrawableImageViewTarget
+import com.luis.montes.ubicaciones.R
 import com.luis.montes.ubicaciones.data.modelos.Recetas
 import com.luis.montes.ubicaciones.databinding.ElementoRecetaBinding
 
@@ -21,20 +22,18 @@ class RecetasHolder(
             listener(receta)
         }
 
-        val imagen= receta.imagenes[0].UrlImagen
 
-        //Glide.with(vistaAsociada.imagenReceta.context)
-         //   .load(receta.imagenes[0])
-         //   .into(vistaAsociada.imagenReceta)
+        val objetoImagen=receta.imagenes.filter{it-> it.isPortada} //Seleccionando la imagen portada del listado
+        val urlImagen= objetoImagen.let{
+            it[0].UrlImagen
+        }
 
-
-
-        Glide.with(vistaAsociada.imagenReceta.context)
-            .load(imagen)
-            .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
-            .into<DrawableImageViewTarget>(DrawableImageViewTarget(vistaAsociada.imagenReceta))
-
-
+       if(urlImagen!=null) { // urlImagen.let { }
+           Glide.with(vistaAsociada.imagenReceta.context)
+               .load(urlImagen)
+               .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC))//Que Glide decida o NONE para que no guarde cache
+               .into<DrawableImageViewTarget>(DrawableImageViewTarget(vistaAsociada.imagenReceta))
+       }
     }
 
 }
